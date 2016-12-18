58714ac60b1cda0d8f2a4dd0570023297b515d1e
Ajinkya Kshirsagar <ajinkya.tesla@gmail.com>
2016-12-18 11:18:28 -0800

    allow adding audio to question

--- app/src/main/java/co/minesweepers/birthday/services/DataUploaderService.java
+++ app/src/main/java/co/minesweepers/birthday/services/DataUploaderService.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -19,11 +20,13 @@ public class DataUploaderService {
     public interface ResponseHandler {
         void onSuccess(String objectPath);
 
+        void onProgress(long totalBytes, long bytesSent);
+
         void onFailure();
     }
 
 
-    public static void uploadData(final Uri uri, final ResponseHandler handler) throws FileNotFoundException {
+    public static void uploadData(final Uri uri, final ResponseHandler handler) {
 
         StorageReference storageRef = FirebaseStorage.getInstance().getReference();
         final String randomisedFilename = UUID.randomUUID().toString() + uri.getLastPathSegment();
@@ -36,6 +39,12 @@ public class DataUploaderService {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         handler.onSuccess(objectPath);
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        handler.onProgress(taskSnapshot.getTotalByteCount(), taskSnapshot.getBytesTransferred());
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
