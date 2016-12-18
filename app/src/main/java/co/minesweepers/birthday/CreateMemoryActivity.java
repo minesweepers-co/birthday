b9c25a54736314ec2567d364f9e178a70c4d0435
Harsh <shahmharsh@gmail.com>
2016-12-17 17:30:48 -0800

    Add two activities for View and Create
    - Added two buttons on main activity to launch view and create activities

---
+++ app/src/main/java/co/minesweepers/birthday/CreateMemoryActivity.java
@@ -0,0 +1,13 @@
+package co.minesweepers.birthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMemoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_memory);
+    }
+}
--- app/src/main/java/co/minesweepers/birthday/MainActivity.java
+++ app/src/main/java/co/minesweepers/birthday/MainActivity.java
@@ -1,13 +1,38 @@
 package co.minesweepers.birthday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewCreate;
+    private TextView mTextViewView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextViewCreate = (TextView) findViewById(R.id.create);
+        mTextViewCreate.setOnClickListener(this);
+        mTextViewView = (TextView) findViewById(R.id.view);
+        mTextViewView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.create:
+                Intent createMemoryIntent = new Intent(this, CreateMemoryActivity.class);
+                startActivity(createMemoryIntent);
+                break;
+            case R.id.view:
+                Intent viewMemoryIntent = new Intent(this, ViewMemoryActivity.class);
+                startActivity(viewMemoryIntent);
+                break;
+        }
     }
 }
