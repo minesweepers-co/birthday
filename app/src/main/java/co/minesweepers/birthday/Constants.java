8c585d2bbdf50ddcc6d768232933076ca9229dee
Harsh Shah <shahmharsh@gmail.com>
2017-04-09 17:53:51 -0700

    Add PersonWelcomeActivity according to UI specs (#20)

--- app/src/main/java/co/minesweepers/birthday/Constants.java
+++ app/src/main/java/co/minesweepers/birthday/Constants.java
@@ -2,4 +2,5 @@ package co.minesweepers.birthday;
 
 public class Constants {
     public static final String INTENT_EXTRA_KEY_PERSON_ID = "personId";
+    public static final String AMATIC_FONT = "fonts/amatic_bold.ttf";
 }
--- app/src/main/java/co/minesweepers/birthday/activities/MainActivity.java
+++ app/src/main/java/co/minesweepers/birthday/activities/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(createMemoryIntent);
                 break;
             case R.id.button_view:
-                Intent viewMemoryIntent = new Intent(this, ViewMemoryActivity.class);
-                startActivity(viewMemoryIntent);
+                Intent welcomePersonIntent = new Intent(this, PersonWelcomeActivity.class);
+                startActivity(welcomePersonIntent);
                 break;
         }
     }
