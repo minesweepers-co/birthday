76347f70e20d28a542d1a6b1e1cad5ac3c37f492
Harsh <shahmharsh@gmail.com>
2017-04-11 18:31:19 -0700

    Basic implementation of viewing memory
    - Currently only one person is shown
    - UI still not according to specs
    - Only questions are shown

--- app/src/main/java/co/minesweepers/birthday/activities/ViewMemoryActivity.java
+++ app/src/main/java/co/minesweepers/birthday/activities/ViewMemoryActivity.java
@@ -1,13 +1,19 @@
 package co.minesweepers.birthday.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 import co.minesweepers.birthday.R;
+import co.minesweepers.birthday.adapters.ViewMemoryAdapter;
 import co.minesweepers.birthday.model.Memory;
+import co.minesweepers.birthday.model.Person;
 
 public class ViewMemoryActivity extends AppCompatActivity {
 
@@ -15,17 +21,34 @@ public class ViewMemoryActivity extends AppCompatActivity {
 
     private static final String DUMMY_JSON = "{\"id\" : \"memory_id\", \"people\":[ {\"id\":\"person_id\", \"name\":\"Harsh\", \"videoUri\":\"harsh_video\", \"questions\":[ { \"question\":\"How old do you think you really are?\", \"option1\":\"15\", \"option2\":\"16\", \"option3\":\"17\", \"correctOption\":\"2\", \"audioUri\":\"harsh_q1_audio\" }, { \"question\":\"Most memorable thing that happened in Subrata Roy Sahara stadium?\", \"option1\":\"Watching the cricket game\", \"option2\":\"Someone drinking free alcohol\", \"option3\":\"Waiting in line in the ccar for more than a hour and listening to songs\", \"correctOption\":\"2\", \"audioUri\":\"harsh_q2_audio\" }, { \"question\":\"What game can you play all day long(And always lose to Bhai)?\", \"option1\":\"Game Of Life\", \"option2\":\"Teen Patti\", \"option3\":\"Bukharo\", \"correctOption\":\"3\", \"audioUri\":\"harsh_q3_audio\" }, { \"question\":\"When and where were you most surprised?\", \"option1\":\"At home, around Diwali 2015 post midnight\", \"option2\":\"Outside FC around christmas 2014\", \"option3\":\"At home on your birthday\", \"correctOption\":\"1\", \"audioUri\":\"harsh_q4_audio\" } ] } ] }";
 
+    private List<Person> mPeople;
+    private RecyclerView mRecyclerView;
+    private ViewMemoryAdapter mAdapter;
+    private int mCurrentPersonIndex = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_memory);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.view_memory_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new ViewMemoryAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        populatePeople();
+        if (mPeople != null && mPeople.size() > 0) {
+            mCurrentPersonIndex = 0;
+            Person person = mPeople.get(mCurrentPersonIndex);
+            mAdapter.setPerson(person);
+        } else {
+            Log.e(TAG, "No people retrieved from server");
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void populatePeople() {
         try {
             Memory memory = Memory.fromJson(DUMMY_JSON);
+            mPeople = memory.getAllPeople();
             Log.d(TAG, "Memory " + memory.getId() + " successfully created from JSON");
         } catch (JSONException e) {
             Log.e(TAG, "JSON ERROR: " + e.getLocalizedMessage());
---
+++ app/src/main/java/co/minesweepers/birthday/adapters/ViewMemoryAdapter.java
@@ -0,0 +1,123 @@
+package co.minesweepers.birthday.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import co.minesweepers.birthday.R;
+import co.minesweepers.birthday.Utils;
+import co.minesweepers.birthday.model.Person;
+import co.minesweepers.birthday.model.Question;
+
+public class ViewMemoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int NAME_CELL_OFFSET = 1;
+    private static final int VIEW_TYPE_NAME = 0;
+    private static final int VIEW_TYPE_QUESTION = 1;
+
+    private Person mPerson;
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return VIEW_TYPE_NAME;
+        }
+
+        return VIEW_TYPE_QUESTION;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_NAME) {
+            return new ViewMemoryAdapter.NameViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_memory_name_cell, parent, false));
+        }
+
+        if (viewType == VIEW_TYPE_QUESTION) {
+            return new ViewMemoryAdapter.QuestionViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_memory_question_cell, parent, false));
+        }
+
+        throw new IllegalStateException("Invalid view type: " + viewType);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == VIEW_TYPE_NAME) {
+            ((NameViewHolder) holder).bind(mPerson.getName());
+            return;
+        }
+
+        ((QuestionViewHolder) holder).bind(mPerson.getAllQuestions().get(position - NAME_CELL_OFFSET));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mPerson == null) {
+            return 0;
+        }
+
+        return mPerson.getAllQuestions().size() + NAME_CELL_OFFSET;
+    }
+
+    public void setPerson(Person person) {
+        mPerson = person;
+        notifyDataSetChanged();
+    }
+
+    private class NameViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mName;
+
+        NameViewHolder(View itemView) {
+            super(itemView);
+            mName = (TextView) itemView.findViewById(R.id.text_view_name);
+        }
+
+        void bind(String name) {
+            mName.setText(name);
+        }
+    }
+
+    private class QuestionViewHolder extends RecyclerView.ViewHolder {
+
+        private List<RadioButton> mRadioButtons;
+        private RadioGroup mRadioGroup;
+        private TextView mTextViewQuestion;
+
+        private Question mQuestion;
+
+        QuestionViewHolder(View itemView) {
+            super(itemView);
+
+            mTextViewQuestion = (TextView) itemView.findViewById(R.id.text_view_question);
+            mRadioGroup = (RadioGroup) itemView.findViewById(R.id.radio_group);
+            RadioButton radioButton1 = (RadioButton) itemView.findViewById(R.id.radio_option1);
+            RadioButton radioButton2 = (RadioButton) itemView.findViewById(R.id.radio_option2);
+            RadioButton radioButton3 = (RadioButton) itemView.findViewById(R.id.radio_option3);
+            RadioButton radioButton4 = (RadioButton) itemView.findViewById(R.id.radio_option4);
+
+            mRadioButtons = Arrays.asList(radioButton1, radioButton2, radioButton3, radioButton4);
+        }
+
+        void bind(Question question) {
+            mQuestion = question;
+            mTextViewQuestion.setText(mQuestion.getQuestion());
+            List<String> options = mQuestion.getOptions();
+            for (int i=0; i < options.size(); i++) {
+                String option = options.get(i);
+                RadioButton radioButton = mRadioButtons.get(i);
+                if (!Utils.isEmpty(option)) {
+                    radioButton.setText(option);
+                } else {
+                    radioButton.setVisibility(View.GONE);
+                }
+            }
+        }
+    }
+}
