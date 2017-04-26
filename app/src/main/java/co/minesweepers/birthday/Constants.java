edee0f1413031ab04c4c32c49bbe3c91bfef72f3
Ajinkya Kshirsagar <ajinkya.tesla@gmail.com>
2017-04-26 06:26:17 -0700

    add metrics for finished created memory

--- app/src/main/java/co/minesweepers/birthday/Constants.java
+++ app/src/main/java/co/minesweepers/birthday/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final String EVENT_ID_APP_LAUNCH = "app_launch";
     public static final String EVENT_ID_CREATE_MEMORY = "create_memory";
     public static final String EVENT_ID_VIEW_MEMORY = "view_memory";
+
+    public static final String EVENT_ID_FINISH_CREATING_MEMORY = "create_memory_done";
 }
