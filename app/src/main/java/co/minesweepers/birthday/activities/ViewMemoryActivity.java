7baa02490be531be6d9bef678109433db6913f1f
Harsh <shahmharsh@gmail.com>
2017-05-01 19:53:18 -0700

    Use memory instance to view memory

--- app/src/main/java/co/minesweepers/birthday/activities/ViewMemoryActivity.java
+++ app/src/main/java/co/minesweepers/birthday/activities/ViewMemoryActivity.java
@@ -20,9 +20,7 @@ import it.sephiroth.android.library.tooltip.Tooltip;
 public class ViewMemoryActivity extends AppCompatActivity implements ViewMemoryAdapter.Listener {
 
     private static final String TAG = "ViewMemoryActivity";
-
-    private static final String DUMMY_JSON = "{\"id\" : \"memory_id\", \"people\":[ {\"id\":\"person_id1\", \"name\":\"Harsh\", \"videoUri\":\"harsh_video\", \"questions\":[ { \"question\":\"What game can you play all day long(And always lose to Bhai)?\", \"option1\":\"Game Of Life\", \"option2\":\"Teen Patti\", \"option3\":\"Bukharo\", \"correctOption\":\"2\", \"audioUri\":\"harsh_q3_audio\" }, { \"question\":\"When and where were you most surprised?\", \"option1\":\"At home, around Diwali 2015 post midnight\", \"option2\":\"Outside FC around christmas 2014\", \"option3\":\"At home on your birthday\", \"correctOption\":\"0\", \"audioUri\":\"harsh_q4_audio\" } ] }, {\"id\":\"person_id2\", \"name\":\"Ajinkya\", \"videoUri\":\"harsh_video\", \"questions\":[ { \"question\":\"How old do you think you really are?\", \"option1\":\"15\", \"option2\":\"16\", \"option3\":\"17\", \"correctOption\":\"1\", \"audioUri\":\"harsh_q1_audio\" }, { \"question\":\"Most memorable thing that happened in Subrata Roy Sahara stadium?\", \"option1\":\"Watching the cricket game\", \"option2\":\"Someone drinking free alcohol\", \"option3\":\"Waiting in line in the ccar for more than a hour and listening to songs\", \"correctOption\":\"1\", \"audioUri\":\"harsh_q2_audio\" } ] } ] }";
-
+    
     private List<Person> mPeople;
     private RecyclerView mRecyclerView;
     private ViewMemoryAdapter mAdapter;
@@ -49,13 +47,9 @@ public class ViewMemoryActivity extends AppCompatActivity implements ViewMemoryA
     }
 
     private void populatePeople() {
-        try {
-            Memory memory = Memory.fromJson(DUMMY_JSON);
-            mPeople = memory.getAllPeople();
-            Log.d(TAG, "Memory " + memory.getId() + " successfully created from JSON");
-        } catch (JSONException e) {
-            Log.e(TAG, "JSON ERROR: " + e.getLocalizedMessage());
-        }
+        Memory memory = Memory.getInstance();
+        mPeople = memory.getAllPeople();
+        Log.d(TAG, "Memory " + memory.getId() + " successfully created from JSON");
     }
 
     @Override
